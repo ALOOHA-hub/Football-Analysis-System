b35078cdcc3c19038cebc970d2d476cb16458f3f
alaa.zreekie <alaa.zreekie@hiast.edu.sy>
2026-02-10 14:11:58 +0300

    Feature: Add configuration loader and use it in the inference of yolo

--- yolo_inference.py
+++ yolo_inference.py
@@ -1,8 +1,17 @@
 from ultralytics import YOLO
 
-model = YOLO("yolov8x")
+from utils.config_loader import cfg
 
-results = model.predict("./data/input/08fd33_4.mp4", save=True)
+model_path = cfg['settings']['model_path']
+input_video_path = cfg['settings']['input_video_path']
+conf_thres = cfg['settings']['confidence_threshold']
+iou_thres = cfg['settings']['iou_threshold']
+output_path = cfg['settings']['output_path']
+
+
+model = YOLO(model_path)
+
+results = model.predict(input_video_path, save=True, project=output_path, name="inference", exist_ok=True, conf=conf_thres, iou=iou_thres,output=output_path)
 print(results[0])
 print("===========================")
 for box in results[0].boxes:
