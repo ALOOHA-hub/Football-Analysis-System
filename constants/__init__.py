7fe7d3f666326596526d4310b275b540c11f90f9
alaa.zreekie <alaa.zreekie@hiast.edu.sy>
2026-02-11 14:29:25 +0300

    Featre: Add consts files that the project will follow instead of hard codded value

---
+++ constants/__init__.py
@@ -0,0 +1,5 @@
+from .annotator_consts import *
+from .detector_consts import *
+from .tracker_consts import *
+from .team_assigner_consts import *
+from .video_utils_consts import *
---
+++ constants/tracker_consts.py
@@ -0,0 +1,5 @@
+# Tracker Class Labels
+CLASS_PLAYER = "player"
+CLASS_REFEREE = "referee"
+CLASS_BALL = "ball"
+CLASS_GOALKEEPER = "goalkeeper"
