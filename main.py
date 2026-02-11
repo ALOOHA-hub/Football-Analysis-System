011ad32137f9b51ad86413e26e122e07bf95a52c
alaa.zreekie <alaa.zreekie@hiast.edu.sy>
2026-02-11 11:15:50 +0300

    Feature: Detection of players, ball and referee sucessfuly after implementing the elipces logic

--- main.py
+++ main.py
@@ -7,21 +7,19 @@ def main():
     video_path = cfg['settings']['input_video_path']
     save_path = os.path.join(cfg['settings']['output_path'], 'output_video.mp4')
     model_path = cfg['settings']['model_path']
+    stub_path = cfg['settings']['stub_path']
 
-    if not os.path.exists(cfg['settings']['output_path']):
-        os.makedirs(cfg['settings']['output_path'])
     frames = read_video(video_path)
 
     #Init Tracker
     tracker = Tracker(model_path)
-    tracks = tracker.get_object_tracks(frames)
-    print(tracks)
-
-    #save video with tracks
+        
+    tracks = tracker.get_object_tracks(frames,
+                                       read_from_stub=True,
+                                       stub_path=stub_path)
+    # Save video with tracks
     frames = tracker.draw_annotations(frames, tracks)
     save_video(frames, save_path)
-    fps, width, height = get_video_properties(video_path)
-    print(f'FPS: {fps}, Width: {width}, Height: {height}')
 
 if __name__ == "__main__":
     main()
